43aef21732fd73ed085f7b25a7211906b150e89f
mnn281 <mananpreetsingh281@gmail.com>
2025-02-24 14:55:40 +0000

    typeof-checking

---
+++ console.js
@@ -0,0 +1,7 @@
+const bigNumber = 123456789n
+const outsideTEmp = null
+const array = ["1", "2", "3", "4", "5", "6",];
+const func = function() {
+    // console.log("Hello World");
+};
+console.log(typeof func);
